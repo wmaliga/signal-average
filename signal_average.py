cb9208bed06cad4393c43a44943e34ed519da31e
wmaliga <wojciech.maliga@gmail.com>
2022-03-13 16:30:13 +0100

    Application basic initialization

--- signal_average.py
+++ signal_average.py
@@ -1,5 +1,14 @@
+import sys
+
+
 def main():
+    if len(sys.argv) < 1:
+        print('Signal Average: no workbook provided')
+
+    workbook_path = sys.argv[1]
+
     print('Signal Average')
+    print('Processing workbook: %s' % workbook_path)
 
 
 if __name__ == '__main__':
