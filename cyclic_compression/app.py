197a291b07f588402ecd768183b71400c1eab6bf
wmaliga <wojciech.maliga@gmail.com>
2022-10-04 19:47:44 +0200

    Plot steps refactor

--- cyclic_compression/app.py
+++ cyclic_compression/app.py
@@ -11,7 +11,6 @@ colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
 
 def main():
     print('Cyclic compression calculations')
-    save_steps = True
 
     for workbook_name, sheets in config.items():
         workbook = open_workbook(f'data/{workbook_name}')
@@ -23,14 +22,15 @@ def main():
 
             for dataset_n, dataset in sheet.items():
                 print(f'Processing dataset #{dataset_n}: {str(list(dataset.values()))}')
+                plot_steps = dataset.get('plot', False)
+
                 t_series = workbook.load_single_series(Cell(dataset['t']))
                 e_series = workbook.load_single_series(Cell(dataset['E']))
                 l_series = workbook.load_single_series(Cell(dataset['L']))
                 minima, _ = find_peaks(-e_series)
-                if save_steps:
+                if plot_steps:
                     plt.plot(t_series, e_series)
                     plt.plot(t_series[minima], e_series[minima], '.')
-                    plt.savefig('out/step1.png')
                     plt.show()
 
                 t_series_list = numpy.split(t_series, minima)
@@ -44,19 +44,17 @@ def main():
                 e_series_groups = list(chunks(e_series_list, 5))
                 l_series_groups = list(chunks(l_series_list, 5))
 
-                if save_steps:
+                if plot_steps:
                     for i in range(len(t_series_groups)):
                         color = colors[i % len(colors)]
                         for j in range(len(t_series_groups[i])):
                             plt.plot(t_series_groups[i][j], e_series_groups[i][j], color)
-                    plt.savefig('out/step2e.png')
                     plt.show()
 
                     for i in range(len(t_series_groups)):
                         color = colors[i % len(colors)]
                         for j in range(len(t_series_groups[i])):
                             plt.plot(t_series_groups[i][j], l_series_groups[i][j], color)
-                    plt.savefig('out/step2l.png')
                     plt.show()
 
                 t_series_groups = t_series_groups[1:]
@@ -67,17 +65,15 @@ def main():
                     group_start_t = t_series_groups[i][0][0]
                     t_series_groups[i] = [t_series - t_series[0] + group_start_t for t_series in t_series_groups[i]]
 
-                if save_steps:
+                if plot_steps:
                     for i in range(len(t_series_groups)):
                         for j in range(len(t_series_groups[i])):
                             plt.plot(t_series_groups[i][j], e_series_groups[i][j])
-                    plt.savefig('out/step3e.png')
                     plt.show()
 
                     for i in range(len(t_series_groups)):
                         for j in range(len(t_series_groups[i])):
                             plt.plot(t_series_groups[i][j], l_series_groups[i][j])
-                    plt.savefig('out/step3l.png')
                     plt.show()
 
                 t_avg_groups = []
@@ -101,15 +97,13 @@ def main():
                     t_avg_groups[i] = [t - t_avg_groups[i][0] + prev_t_end for t in t_avg_groups[i]]
                     prev_t_end = t_avg_groups[i][-1]
 
-                if save_steps:
+                if plot_steps:
                     for i in range(len(t_avg_groups)):
                         plt.plot(t_avg_groups[i], e_avg_groups[i])
-                    plt.savefig('out/step4e.png')
                     plt.show()
 
                     for i in range(len(t_avg_groups)):
                         plt.plot(t_avg_groups[i], l_avg_groups[i])
-                    plt.savefig('out/step4l.png')
                     plt.show()
 
                 t_avg_values = []
@@ -125,13 +119,11 @@ def main():
                     l_avg_values.extend(l_avg_groups[i])
                     l_dev_values.extend(l_dev_groups[i])
 
-                if save_steps:
+                if plot_steps:
                     plt.plot(t_avg_values, e_avg_values)
-                    plt.savefig('out/step5e.png')
                     plt.show()
 
                     plt.plot(t_avg_values, l_avg_values)
-                    plt.savefig('out/step5l.png')
                     plt.show()
 
                 sheet_name_avg = f'{sheet_name}_avg'
@@ -139,8 +131,6 @@ def main():
                 workbook.write_single_series(Cell(dataset['t']), t_avg_values, override_sheet=sheet_name_avg)
                 workbook.write_single_series(Cell(dataset['E']), e_avg_values, override_sheet=sheet_name_avg)
                 workbook.write_single_series(Cell(dataset['L']), l_avg_values, override_sheet=sheet_name_avg)
-
-                save_steps = False
 
         save_with_suffix(workbook, "new")
 
